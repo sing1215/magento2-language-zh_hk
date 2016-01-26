0b4694099e2433786cbf749fae02f2cc5f926151
Clarence Ho <clarence@skywidesoft.com>
2016-01-26 13:33:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# magento2-language-zh_hk
+
+Magento 2 language pack - Chinese (Hong Kong)
+
+> This package is still under development.
+
+Testing comment
---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Magento 2 Chinese (Hong Kong) language pack
+ *
+ * @category   Language
+ * @package    zh_HK
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author     Clarence Ho (http://www.skywidesoft.com/)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'skywidesoft_zh_hk',
+    __DIR__
+);
